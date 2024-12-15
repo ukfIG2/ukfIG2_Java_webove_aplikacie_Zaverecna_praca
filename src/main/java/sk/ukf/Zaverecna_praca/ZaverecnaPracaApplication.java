6b8ab49861f1fffbc9cff9befb0f178c89da93f3
van <van@vam>
2024-12-15 10:11:00 +0100

    Prvy commit

---
+++ src/main/java/sk/ukf/Zaverecna_praca/ZaverecnaPracaApplication.java
@@ -0,0 +1,13 @@
+package sk.ukf.Zaverecna_praca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaverecnaPracaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZaverecnaPracaApplication.class, args);
+	}
+
+}
